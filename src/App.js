d4f5af0ea6580d708e6bdc4d56c818994892ccb9
onpilot <72357530+onpilot@users.noreply.github.com>
2022-01-26 22:30:26 +0700

    update initial layout and core functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // const [value, setValue] = useState(false);
+  const playSound = (padLetter) => {
+    const audio = document.getElementById(padLetter);
+    try {
+      audio.play();
+    } catch (error) {
+      console.log('No Id found');
+    }
+  };
   return (
-    <>
-      <div>initial setup</div>
-    </>
+    <div id="drum-machine">
+      <div id="pads" className="wrapper">
+        <div className="drum-pad">
+          <input
+            type="button"
+            className="drum-pad-letter"
+            value="q"
+            onClick={() => playSound('q')}
+          />
+          <audio
+            id="q"
+            className="clip"
+            src="https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+          ></audio>
+        </div>
+        <div className="drum-pad"></div>
+        <div className="drum-pad"></div>
+        <div className="drum-pad"></div>
+        <div className="drum-pad"></div>
+        <div className="drum-pad"></div>
+        <div className="drum-pad"></div>
+        <div className="drum-pad"></div>
+        <div className="drum-pad"></div>
+      </div>
+      <div id="display">
+        <div className="power">p</div>
+        <div className="bank">b</div>
+      </div>
+    </div>
   );
 }
 
