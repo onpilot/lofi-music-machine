f67605a9943b95ba0f82eb095a788989d5baf0e9
onpilot <72357530+onpilot@users.noreply.github.com>
2022-01-30 01:04:15 +0700

    change audio files list to dictionary

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,20 +1,26 @@
 import './drum-pad.css';
-import audioList from './audioList.js';
+import audioDictionary from './sounds.js';
 
 const Audio = (props) => {
-  const audioSrc = audioList[0][props.id];
-  // console.log(audioSrc);
-  return <audio id={props.id} className="clip" src={audioSrc}></audio>;
+  const { id } = props;
+  // const audioSrc = audioList[0][id];
+  // const audioSrc = audioDictionary[id]['src'];
+  const { src, name } = audioDictionary[id];
+  return (
+    <audio id={id} className="clip" src={src}>
+      {name}
+    </audio>
+  );
 };
 
 const DrumPad = (props) => {
-  const id = props.id;
+  const { id, onClick } = props;
   return (
     <button
       id={`pad-${id}`}
       className="drum-pad pad-letter"
       type="button"
-      onClick={props.onClick}
+      onClick={onClick}
     >
       {id}
       <Audio id={id}></Audio>
